435c093ecd2d9cdeec50e0d3eef7e96646158b66
bula <anuar.b@chocomart.kz>
2022-03-28 20:25:30 +0600

    new productFilter and categories

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -11,27 +11,27 @@
     </h3>
 
     <span class="catalog__price">
-              {{ product.price }} ₽
-            </span>
+      {{ product.price }} ₽
+    </span>
 
     <ul class="colors colors--black">
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="colorZ">
+          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color">
           <span class="colors__value" style="background-color: #73B6EA;">
                   </span>
         </label>
       </li>
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000" v-model="colorZ">
+          <input class="colors__radio sr-only" type="radio" value="#8BE000" v-model="color">
           <span class="colors__value" style="background-color: #8BE000;">
                   </span>
         </label>
       </li>
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#222" v-model="colorZ">
+          <input class="colors__radio sr-only" type="radio" value="#222" v-model="color">
           <span class="colors__value" style="background-color: #222;">
                   </span>
         </label>
